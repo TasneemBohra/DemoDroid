163ef08933b34400b2256bc6e7b5bcd78f882bda
TasneemBohra <tasneem.bohra@hotmail.com>
2017-04-19 12:35:25 +0530

    Project setup

---
+++ app/src/main/java/com/tasneembohra/demodroid/databinding/Demo.java
@@ -0,0 +1,8 @@
+package com.tasneembohra.demodroid.databinding;
+
+/**
+ * Created by tasneem on 19/04/17.
+ */
+
+public class Demo {
+}
---
+++ app/src/main/java/com/tasneembohra/demodroid/performance/Demo.java
@@ -0,0 +1,8 @@
+package com.tasneembohra.demodroid.performance;
+
+/**
+ * Created by tasneem on 19/04/17.
+ */
+
+public class Demo {
+}
